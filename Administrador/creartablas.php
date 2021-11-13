6d31111e76ac57564b5ac4cdac87cb5d423f21d7
jdpedrazat <79552926+jdpedrazat@users.noreply.github.com>
2021-11-13 14:39:01 -0500

    Update creartablas.php

--- Administrador/creartablas.php
+++ Administrador/creartablas.php
@@ -1,24 +1,38 @@
 <?php
+    
     $servidor = "localhost";
     $nombreusuario = "root";
     $password = "12345678";
-    $database = "bdunad301127_14";
+    $dbnombre = "bdunad301127_14";
     
     // Crear connection
-    $conexion = new mysqli($servidor, $nombreusuario, $password, $database);
-
+    $conexion = new mysqli($servidor, $nombreusuario, $password, $dbnombre);
+    echo "<a href='../index.html' text-aling='center'> Volver</a>";
+    echo "<br>";
     // Check connection
     if ($conexion->connect_error){
         die("Error de conexión al Servidor: " . $conexion->connect_error);
+        echo "<a href='../index.html'> Volver</a>";
     }
-    
+
     //Crear Tabla
-    $sql = "CREATE DATABASE bdunad301127_14";
+    $sql = "CREATE TABLE productos (
+        id SMALLINT auto_increment PRIMARY KEY,
+        codigobarras VARCHAR(40),
+        nombre VARCHAR(60) NOT NULL,
+        fechaalta TIMESTAMP,
+        valorneto DOUBLE,
+        ganancia FLOAT,
+        cantidad int,
+        proveedor SMALLINT,
+        descripcion BLOB)
+        ";
     if($conexion->query($sql) === true){
-        echo "Base de datos creada satisfactoriamente...";
+        echo "Tabla creada satisfactoriamente...";
     }else{
-        die("Error al Crear Base de Datos" . $conexion->error);
+        die("Error al Tabla en la Base de Datos" . $conexion->error);
     }
     //cerrar conexión
     mysqli_close($conexion);
+    
 ?>
